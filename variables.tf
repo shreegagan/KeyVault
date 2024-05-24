b4eb500f636c8a4ad3ff7deadd5236fafe0ef337
shreegagan <104724964+shreegagan@users.noreply.github.com>
2024-05-24 19:08:19 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+    type = string
+  
+}
+variable "location" {
+    type = string
+  
+}
+variable "azurerm_key_vault" {
+    type = string
+
+}
+variable "azurerm_key_vault_secret" {
+    type = string  
+}
+variable "azurerm_key_vault_value" {
+    type = string
+  
+}
